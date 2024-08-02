9b73d69a994714fdbb2ea2783f8faa2050c05157
leonidig <leonid200955@gmail.com>
2024-08-03 01:46:22 +0300

    first commit

---
+++ lesson1/index.js
@@ -0,0 +1,5 @@
+console.log(typeof(10))  // number
+console.log(typeof(1.0)) // number
+console.log(typeof(true)) // boolean
+console.log(typeof("Aboba press f")) // string
+console.log(typeof([1, 2, 3])) // object
